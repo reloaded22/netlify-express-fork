8613c86a48c3ed5df25134d503ba2caf3927b1f8
reloaded22 <andres.rojas.talent@gmail.com>
2022-10-30 22:18:43 -0500

    Other final tests

--- server-local.js
+++ server-local.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log("\n***The server-local.js file is being run***");
+console.log("\n***Calling the server.js file from server-local.js***");
 const app = require('./express/server');
 console.log("\n***This is run after requiring the server.js file***");
 
